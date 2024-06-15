6c4733ad357d92b7823443305bf0d2fa3ecbdd63
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2024-06-15 20:09:07 -0300

    15-06

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,41 @@
+import { useState } from 'react'
 import '../styles/Home.css'
 
 const Home = () => {
+
+    const [url, setUrl] = useState('')
+    const [shorted, setShorted] = useState(false)
+
+    const handle = (e: any) => {
+        e.preventDefault()
+        if (!shorted) {
+            console.log(url)
+            setShorted(true)
+            setUrl(window.location.origin + "/adsfsadfsda")
+        } else {
+            navigator.clipboard.writeText(url)
+            setUrl('')
+            setShorted(false)
+        }
+    }
+
     return (
         <div className='box'>
             <h1>CaminhoCurto</h1>
-            <form>
-                <input type="text" name="url" placeholder="URL" />
-                <input type="submit" value="Encurtar" />
+            <form onSubmit={handle}>
+                <input 
+                    className=''
+                    type="text"
+                    name="url"
+                    placeholder="URL"
+                    value={url}
+                    onChange={e => setUrl(e.target.value)}
+                    disabled={shorted}
+                />
+                <input
+                    type="submit"
+                    value={shorted ? 'Copiar' : 'Encurtar'}
+                />
             </form>
         </div>
     )
